331bac85b65efa8ef5fc4be6f4d581681d2d6d18
Zoltan <zdebre@gmail.com>
2016-05-18 22:07:00 +1200

    Add Router to the App

--- 4_th/app/app.js
+++ 4_th/app/app.js
@@ -1,5 +1,23 @@
 require(['jquery', 'ember'], function ($, Ember) {
   $(function() {
-    Ember.Application.create();
+
+    var Router = Ember.Router.extend({
+      location: 'hash'
+    });
+
+    Router.map(function() {
+      this.route('about');
+    });
+
+    Ember.Application.create({
+      name: 'App',
+      LOG_RESOLVER: true,
+      LOG_ACTIVE_GENERATION: true,
+      LOG_TRANSITIONS: true,
+      LOG_TRANSITIONS_INTERNAL: true,
+      LOG_VIEW_LOOKUPS: true,
+
+      Router: Router
+    });
   });
 });
